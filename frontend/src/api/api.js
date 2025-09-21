2e000a96d483f4d10d5b56198f9bfcf0ce3d889c
Jeong Sohyeon <sohyunj67@gmail.com>
2025-09-21 23:21:41 +0900

    로그인 요청시 토큰 제외

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,19 +4,23 @@ const api = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_API_BASE_URL}`,
 });
 
-// api.interceptors.request.use(config => {
-//     const token = localStorage.getItem("accessToken");
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-// });
+api.interceptors.request.use(config => {
+
+  // 로그인 요청이면 토큰을 보내지 않음
+  if (!config.url.endsWith("/login")) {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
 
 api.interceptors.response.use(
   response => response,
   error => {
-    const loginRequest = error.config.url.includes("/login");
-    if (!loginRequest && error.response && error.response.status == 401) {
+    if (error.response && error.response.status == 401) {
       window.location.href = "/auth/login"; // 토큰 만료시 로그인 페이지 이동
     }
     return Promise.reject(error);
