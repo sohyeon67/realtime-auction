832bbf6f74d38bd857131f0598d2792f89d36e89
Jeong Sohyeon <sohyunj67@gmail.com>
2025-09-25 22:38:39 +0900

    수정

--- backend/src/main/java/com/example/auction/member/dto/MemberIdResDto.java
+++ backend/src/main/java/com/example/auction/member/dto/MemberIdResDto.java
@@ -2,8 +2,9 @@ package com.example.auction.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class MemberIdResDto {
 
