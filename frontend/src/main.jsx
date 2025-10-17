d00d9fa24f3b76d77f9b988ee6d2624fdccfb7b8
Jeong Sohyeon <sohyunj67@gmail.com>
2025-10-17 15:34:36 +0900

    mui 테마 생성

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from '@mui/material'
+import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
 )
---
+++ frontend/src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    black: {
+      main: "#000000",
+      contrastText: "#ffffff",
+    },
+  }
+});
+
+export default theme;
